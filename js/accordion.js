cc7773b651e22ccfa7512b4afa310d4a99c271cd
itsasoalt <itsaso.altuna@gmail.com>
2016-04-20 00:41:58 +0200

    Added files via upload

--- js/accordion.js
+++ js/accordion.js
@@ -1,5 +1,20 @@
+// $(document).ready(function() {
+// 	$('.toggle').click(function() {
+// 		$(this).siblings().toggle('slow');
+// 	});
+// });
+
 $(document).ready(function() {
-	$('.toggle').click(function() {
-		$(this).siblings().toggle('slow');
+	
+	$('.toggle').click(function(e) {
+		if ($(this).siblings().is(':visible')) {
+			$(this).siblings().slideUp();
+			$(this).siblings().find('.inner').slideUp();
+		} else {
+			$(this).parent().parent().find('.inner:visible').slideUp();
+			$(this).siblings('.inner').slideDown();
+		};
+		
 	});
+
 });
